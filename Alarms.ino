0b7222bfd8d9295fbc4661aacc3508e7cf49d53c
tagno25 <tagno25@gmail.com>
2014-12-09 22:15:21 +0000

    LCD Disable in alarms
    forgot to disable some LCD dependent alarms

--- Alarms.ino
+++ Alarms.ino
@@ -11,11 +11,15 @@ void alarm30sec(){
 #ifdef DEBUG
 
 void alarm20sec(){
+  #ifdef LCD
   digitalClockDisplay(); //update LCD display
+  #endif
 }
 
 void alarm60sec(){
+  #ifdef LCD
   switchLCD(); //change LCD display State
+  #endif
 }
 
 #endif
